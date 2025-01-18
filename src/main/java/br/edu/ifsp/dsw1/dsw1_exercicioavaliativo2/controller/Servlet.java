00ea98d72805538ed24cb0fa94a750bfe617c23a
Luiz-frj <luizfrj200412@gmail.com>
2025-01-18 15:45:06 -0300

    Implementação dos servlets

--- src/main/java/br/edu/ifsp/dsw1/dsw1_exercicioavaliativo2/controller/Servlet.java
+++ src/main/java/br/edu/ifsp/dsw1/dsw1_exercicioavaliativo2/controller/Servlet.java
@@ -1,12 +1,13 @@
 package br.edu.ifsp.dsw1.dsw1_exercicioavaliativo2.controller;
 
 import br.edu.ifsp.dsw1.dsw1_exercicioavaliativo2.controller.Command.Command;
+import br.edu.ifsp.dsw1.dsw1_exercicioavaliativo2.controller.Command.IndexCommand;
+import br.edu.ifsp.dsw1.dsw1_exercicioavaliativo2.controller.Command.LoginCommand;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
 import java.io.IOException;
 
 @WebServlet("/principal.do")
@@ -23,9 +24,17 @@ public class Servlet extends HttpServlet {
 
     private void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String action = request.getParameter("action");
-        Command command;
+        Command command = null;
 
+        if("login".equals(action)) {
+            command = new LoginCommand();
+        }else if("index".equals(action)) {
+            command = new IndexCommand();
+        }
 
+        String view = command.execute(request,response);
+        var dispatcher = request.getRequestDispatcher(view);
+        dispatcher.forward(request, response);
     }
 
 }
--- src/main/java/br/edu/ifsp/dsw1/dsw1_exercicioavaliativo2/controller/ServletLogado.java
+++ src/main/java/br/edu/ifsp/dsw1/dsw1_exercicioavaliativo2/controller/ServletLogado.java
@@ -1,6 +1,7 @@
 package br.edu.ifsp.dsw1.dsw1_exercicioavaliativo2.controller;
 
 import br.edu.ifsp.dsw1.dsw1_exercicioavaliativo2.controller.Command.Command;
+import br.edu.ifsp.dsw1.dsw1_exercicioavaliativo2.controller.Command.*;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -22,10 +23,36 @@ public class ServletLogado extends HttpServlet {
     }
 
     private void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Command command = null;
         String action = request.getParameter("action");
-        Command command;
 
+        if("logged".equals(action)) {
+            command = new LogadoCommand();
+        }else if("newUser".equals(action)) {
+            command = new NewUserCommand();
+        }else if("newOrder".equals(action)) {
+            command = new NewOrderCommand();
+        }else if("orders".equals(action)) {
+            command = new OrderCommand();
+        }else if("logout".equals(action)) {
+            command = new LogoutCommand();
+        }else if("createUser".equals(action)) {
+            command = new CreateUserCommand();
+        }else if("createOrder".equals(action)) {
+            command = new NewOrderCommand();
+        }else if("buscarCliente".equals(action)) {
+            command = new BuscaClienteCommand();
+        }else if("update".equals(action)) {
+            command = new UpdateCommand();
+        }else if("commitUpdate".equals(action)) {
+            command = new CommitUpdateCommand();
+        }else if("delete".equals(action)) {
+            command = new DeleteCommand();
+        }
 
+        String view = command.execute(request,response);
+        var dispatcher = request.getRequestDispatcher(view);
+        dispatcher.forward(request, response);
     }
 
 }
